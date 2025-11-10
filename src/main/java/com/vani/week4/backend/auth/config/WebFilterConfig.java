14ad07f2f04f719c1060af877e1607706d3603d4
Superson7 <vin990227@naver.com>
2025-11-10 21:40:57 +0900

    프로덕션 환경 분리 yml등록, URL등록 패턴 개선

--- src/main/java/com/vani/week4/backend/auth/config/WebFilterConfig.java
+++ src/main/java/com/vani/week4/backend/auth/config/WebFilterConfig.java
@@ -3,6 +3,7 @@ package com.vani.week4.backend.auth.config;
 import com.vani.week4.backend.auth.security.SessionAuthFilter;
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,9 @@ import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 웹 요청 필터를 애플리케이션으로 등록하는 스프링 설정 클래스
@@ -20,12 +24,18 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class WebFilterConfig {
     private final SessionAuthFilter sessionAuthFilter;
-
+    @Value("${cors.allowed-origins[0]}")
+    private String allowedOrigins;
     @Bean
     public FilterRegistrationBean<Filter> sessionFilter() {
+
+        List<String> patterns = new ArrayList<>();
+        patterns.add("/*");
+        // TODO DEV 프로피에 따라 다른 패턴 추가가능
+
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(sessionAuthFilter);
-        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setUrlPatterns(patterns);
         filterRegistrationBean.setOrder(1);
         return filterRegistrationBean;
     }
@@ -38,7 +48,7 @@ public class WebFilterConfig {
         //쿠키 교환 가능하게
         config.setAllowCredentials(true);
         //프론트엔트 출처 허용하기
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin(allowedOrigins);
         //허용할 헤더
         config.addAllowedHeader("*");
         //허용할 메서드
